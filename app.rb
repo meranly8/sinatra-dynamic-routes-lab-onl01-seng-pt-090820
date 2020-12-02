729ace15e6520be20ea60f79e1075049a424d9e7
Michelle Ranly <meranly8@gmail.com>
2020-12-02 08:49:42 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,7 +13,10 @@ class App < Sinatra::Base
 
   get '/say/:number/:phrase' do
     @string = ''
-    params[:number].to_i.times {@answer += params[:phrase]}
+    params[:number].to_i.times do
+      @answer += params[:phrase]
+    end
+
     @string
   end
 end
