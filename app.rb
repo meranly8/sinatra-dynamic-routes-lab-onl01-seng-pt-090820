f912213fbf672ac1038a6f0e9e4ff3eaefd276de
Michelle Ranly <meranly8@gmail.com>
2020-12-02 08:44:31 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,8 +3,8 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
   get '/reversename/:name' do
-    @name = params[:name]
-    "#{@name.reverse}"
+    params[:name].reverse
+    
   end
 
   get '/square/:number' do
