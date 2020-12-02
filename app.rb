2734b751a094c7dd3891aefa28987bdfdee853b5
Michelle Ranly <meranly8@gmail.com>
2020-12-02 08:43:22 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -11,4 +11,9 @@ class App < Sinatra::Base
     @square = params[:number].to_i * params[:number].to_i
     "#{@square}"
   end
+
+  get '/say/:number/:phrase' do
+    @phrase_string = params[:number].to_i.times {"#{params[:phrase]}"}
+    "#{@phrase_string}"
+  end
 end
